cdf6ae1edcef625cc834c0b1370a97c78805c8e3
Nathanael <nofight@hotmail.fr>
2014-02-14 18:15:27 +0100

    ajout de lem_room

---
+++ sources/lem_token/lem_room.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "lem-in.h"
+#include "libft.h"
+
+static char		**ft_strtrimsplit(char *line)
+
+void	lem_room(t_map *map, char *line)
+{
+	char	**line;
+	t_lroom	*current;
+
+	if (map->next == TLINK)
+		map->next = TSTOP;
+	else
+	{
+		tmp = ft_strtrimsplit(line);
+		if (!full_digit(tmp[1]) || !full_digit(tmp[2]))
+			lem_error(map, tmp);
+		else
+		{
+			if ((current = get_room(tmp[0])) == NULL)
+				map->map = new_room(tmp, map->map);
+			else
+				current->coo = new_coo(tmp);
+			map->next = TBASIC;
+	}
+}
+
+static char		**ft_strtrimsplit(char *line)
+{
+	char	**tmp;
+	char	*tmp2;
+	tmp2 = ft_strtrim(line);
+	tmp = ft_strsplit(tmp2, ' ');
+	free(tmp2);
+	return (tmp);
+}
