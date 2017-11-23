f1285cf9b4b71425c0fb844fb6b91bda47ca9148
Nathanael PINEAU <npineau@student.42.fr>
2017-11-23 14:39:17 +0100

    we try to do something when unexpected inout is read instead of freeing everything

--- src/main/get_anthill.c
+++ src/main/get_anthill.c
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/24 10:34:43 by npineau           #+#    #+#             */
-/*   Updated: 2017/10/24 11:08:44 by npineau          ###   ########.fr       */
+/*   Updated: 2017/11/23 14:36:41 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,9 @@ int	get_anthill(size_t *ants, t_room **rooms, size_t *rooms_n, t_rb *links)
 		rb_free(links);
 		return (0);
 	}
-	else if (!(check = parse_anthill(STDIN_FILENO, ants, &room_b, links)))
-	{
-		rb_free(&room_b);
-		rb_free(links);
-	}
 	else
 	{
+		check = parse_anthill(STDIN_FILENO, ants, &room_b, links);
 		*rooms = (t_room *)room_b.head;
 		*rooms_n = room_b.used;
 	}
--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: mde-jesu <mde-jesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/19 12:23:39 by mde-jesu          #+#    #+#             */
-/*   Updated: 2017/10/27 10:37:20 by npineau          ###   ########.fr       */
+/*   Updated: 2017/11/23 14:32:40 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,89 +16,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-t_room	*test_create_rooms()
-{
-	t_room	*rooms;
 
-	rooms = (t_room *)malloc(sizeof(t_room) * 4);
-	if (!rooms)
-		return (NULL);
-
-	rooms[0].name = ft_strdup("0");
-	rooms[0].type = START;
-	rooms[0].weight = -1;
-	rb_new(2, sizeof(t_room *), &rooms[0].conn);
-
-	rooms[1].name = ft_strdup("1");
-	rooms[1].type = NORMAL;
-	rooms[1].weight = -1;
-	rb_new(2, sizeof(t_room *), &rooms[1].conn);
-
-	rooms[2].name = ft_strdup("2");
-	rooms[2].type = NORMAL;
-	rooms[2].weight = -1;
-	rb_new(2, sizeof(t_room *), &rooms[2].conn);
-
-	rooms[3].name = ft_strdup("3");
-	rooms[3].type = END;
-	rooms[3].weight = -1;
-	rb_new(2, sizeof(t_room *), &rooms[3].conn);
-
-	return (rooms);
-}
 
 int main()
 {
-	//size_t	ants;
+	size_t	ants;
 	t_room			*rooms;
 	unsigned int	error;
 	size_t			rnb;
 	t_rb			links;
-	t_link			link;
 	t_room			*start;
 
-	//t_link	l;
 
 	error = 0;
-	rnb = 4;
-	rb_new(5, sizeof(t_link), &links);
 
-	link.fst = ft_strdup("0");
-	link.sec = ft_strdup("3");
-	rb_push_back(&links, &link);
-	link.fst = ft_strdup("0");
-	link.sec = ft_strdup("2");
-	rb_push_back(&links, &link);
-	link.fst = ft_strdup("1");
-	link.sec = ft_strdup("3");
-	rb_push_back(&links, &link);
-	link.fst = ft_strdup("1");
-	link.sec = ft_strdup("2");
-	rb_push_back(&links, &link);
-	link.fst = ft_strdup("2");
-	link.sec = ft_strdup("3");
-	rb_push_back(&links, &link);
-
-	rooms = test_create_rooms();
+	get_anthill(&ants, &rooms, &rnb, &links);
 	init_graph(rooms, rnb, links, &error);
-	if (error != 0)
-		what_error(error);
-	print_graph(rooms, rnb);
 	start = find_start_and_error(rooms, rnb, &error);
-	if (error != 0)
+	if (error == 0)
+	{
+		print_graph(rooms, rnb);
+		init_weight(start, &error);
+		print_graph(start, rnb);
+	}
+	else
+	{
 		what_error(error);
-	printf("preinit weight\n");
-	init_weight(start, &error);
-	print_graph(start, rnb);
-
-//	printf("\n----------\n\n");
-//	if (get_anthill(&ants, &rooms, &rnb, &links))
-//	{
-//		init_graph(rooms, rnb, links, &error);
-//		if (error == 0)
-//		{
-//			print_graph(rooms, rnb);
-//		}
-//	}
-//	return (0);
+	}
+	return (0);
 }
