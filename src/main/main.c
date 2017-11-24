fc9671d422f3d6754bb1332fb7e1753dff7c2217
Nathanael PINEAU <npineau@student.42.fr>
2017-11-24 12:40:28 +0100

    slight cleanup

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: mde-jesu <mde-jesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/19 12:23:39 by mde-jesu          #+#    #+#             */
-/*   Updated: 2017/11/24 12:28:51 by npineau          ###   ########.fr       */
+/*   Updated: 2017/11/24 12:40:11 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,21 +32,15 @@ int main(void)
 	get_anthill(&ants, &rooms, &rnb, &links);
 	init_graph(rooms, rnb, links, &error);
 	start = find_start_and_error(rooms, rnb, &error);
-	if ((banana = check_rooms(rooms, rnb, &error)))
+	if (!error && (banana = check_rooms(rooms, rnb, &error)))
 	{
 		end = find_type(END, rooms, rnb);
-		if (!error)
-		{
-			weight_from_end(end);
-			if (start->weight == -1)
-				ft_putendl("ERROR");
-			else
-				transfer_ants(ants, start);
-		}
+		weight_from_end(end);
+		if (start->weight == -1)
+			ft_putendl("ERROR");
 		else
-		{
-			what_error(error);
-		}
+			transfer_ants(ants, start);
+		what_error(error);
 	}
 	else
 	{
